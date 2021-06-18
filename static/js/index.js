2b1fa24a0ea0507736295587e30aca0a20f57079
wain-aina <jesse.wainaina@student.moringaschool.com>
2021-06-18 16:54:33 +0300

    JS part2

--- static/js/index.js
+++ static/js/index.js
@@ -17,3 +17,28 @@ $(document).ready(function(){
   $("#product-mgt").click(function() {
     $(".product-mgt, #product-mgt").toggle("slow");
   });
+  $('.work1').mouseover(function() {
+   $('.work1-overlay').show();
+ }).mouseout(function() {
+   $('.work1-overlay').hide();
+ });
+ $(".work2").mouseenter(function() {
+   $(".work2-overlay").show();
+ }).mouseleave(function() {
+   $(".work2-overlay").hide();
+ });
+ $(".work3").mouseenter(function() {
+   $(".work3-overlay").show();
+ }).mouseleave(function() {
+   $(".work3-overlay").hide();
+ });
+ $(".work4").mouseenter(function() {
+   $(".work4-overlay").show();
+ }).mouseleave(function() {
+   $(".work4-overlay").hide();
+ });
+ $(".work5").mouseenter(function() {
+   $(".work5-overlay").show();
+ }).mouseleave(function() {
+   $(".work5-overlay").hide();
+ });
